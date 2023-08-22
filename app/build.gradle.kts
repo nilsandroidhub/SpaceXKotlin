ea339850bbbe1be6d38fd8b319e487926e2829dd
nilswestregardingvar <nils.westregard.ingvar.work@gmail.com>
2023-08-23 00:37:50 +0200

    Added Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -24,11 +26,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
@@ -43,14 +45,27 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("com.google.code.gson:gson:2.8.7")
-    implementation("com.squareup.retrofit2:retrofit:2.2.0")
-    implementation("com.squareup.retrofit2:adapter-rxjava2:2.2.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.2.0")
     implementation("io.reactivex.rxjava2:rxandroid:2.0.1")
     implementation("io.reactivex.rxjava2:rxjava:2.1.7")
     implementation("com.squareup.picasso:picasso:2.5.2")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    // Coroutine Lifecycle Scopes
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
+    implementation("androidx.activity:activity-ktx:1.6.1")
+
+    //daggerhilt
     implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
     //Glide
     implementation("io.coil-kt:coil:1.4.0")
     implementation("com.github.bumptech.glide:glide:4.14.2")
